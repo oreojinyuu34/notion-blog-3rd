3a22cd39045c03bc56e4ffd35d9c841259a60600
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2023-11-19 12:59:21 +0900

    初期設定完了

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// Next.jsの特別なコンポーネントをインポートします
+import { Html, Head, Main, NextScript } from "next/document";
 
+// Documentコンポーネントをエクスポートします
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    // HTMLドキュメント全体を定義
+    <Html lang="ja">
+      {/* 言語を日本語に設定 */}
+      <Head>
+        {/* ドキュメントの<head>部分。
+            ここにメタデータやスタイルシートのリンクを追加できます */}
+      </Head>
       <body>
+        {/* <Main />はアプリケーションのメインコンテンツがレンダリングされる場所 */}
         <Main />
+
+        {/* <NextScript />はNext.jsが動作するために必要なスクリプトを含みます */}
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
